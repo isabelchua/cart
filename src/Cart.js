71849c73e78117aa70bf64ab2104d6223a31daf5
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-26 23:54:03 -0700

    style change

--- src/Cart.js
+++ src/Cart.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import CartItem from './CartItem';
+import { useState, useEffect } from 'react';
 
 function Cart({ initItems }) {
+	const initialState = JSON.parse(window.localStorage.getItem('items'));
+	const [items, setItems] = useState(initialState || initItems);
+
+	useEffect(() => {
+		window.localStorage.setItem('items', JSON.stringify(items));
+	}, [items]);
+
+	const updateQty = (id, newQty) => {
+		const newItems = items.map(item => {
+			if (item.id === id) {
+				return { ...item, qty: newQty };
+			}
+			return item;
+		});
+		setItems(newItems);
+	};
+
+	const grandTotal = items
+		.reduce((total, item) => total + item.qty * item.price, 0)
+		.toFixed(2);
+
 	return (
 		<div className="cart">
 			<h1>cart</h1>
 			<div className="cart-items">
-				{initItems.map(item => (
-					<CartItem {...item} />
+				{items.map(item => (
+					<CartItem key={item.id} updateQty={updateQty} {...item} />
 				))}
 			</div>
-
+			<h2>Grand Total: $ {grandTotal}</h2>
 			{/* <li>
 					Name: {item.name}, Price: {item.price}
 				</li> */}
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-function CartItem({ id, name, price, qty }) {
+function CartItem({ id, name, price, qty, updateQty }) {
+	const addOne = () => {
+		updateQty(id, qty + 1);
+	};
+	const minusOne = () => {
+		updateQty(id, qty - 1);
+	};
+
 	return (
 		<div className="CartItem">
 			<div>{name}</div>
+			<div>${price}</div>
+			<div className="qty">
+				<button onClick={minusOne} disabled={qty <= 1 ? true : false}>
+					-
+				</button>
+				<div>{qty}</div>
+				<button onClick={addOne}>+</button>
+			</div>
+			<div>Total: ${qty * price}</div>
 		</div>
 	);
 }
